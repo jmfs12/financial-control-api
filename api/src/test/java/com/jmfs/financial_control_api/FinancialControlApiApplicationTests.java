7412f50ec3de037c7411915cc0fd1dd686ea481e
João Miguel Francisco de Souza <joanomiguel@gmail.com>
2025-08-18 12:33:38 -0300

    feat: initial springboot setup

---
+++ api/src/test/java/com/jmfs/financial_control_api/FinancialControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmfs.financial_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
