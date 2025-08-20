e8d2021d30121ded4a698be138e380f5e01fd6c4
João Miguel Francisco de Souza <joanomiguel@gmail.com>
2025-08-20 16:48:49 -0300

    feat: add workflow

--- api/src/main/java/com/jmfs/financial_control_api/FinancialControlApiApplication.java
+++ api/src/main/java/com/jmfs/financial_control_api/FinancialControlApiApplication.java
@@ -3,7 +3,9 @@ package com.jmfs.financial_control_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
 public class FinancialControlApiApplication {
 
 	public static void main(String[] args) {
